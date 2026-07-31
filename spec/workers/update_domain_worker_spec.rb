401f95bdc72b98a182046d597b270b2087751c91
Daniel O'Connor <daniel.oconnor@gmail.com>
2026-07-31 19:19:23 +0930

    Update update_domain_worker_spec.rb

--- spec/workers/update_domain_worker_spec.rb
+++ spec/workers/update_domain_worker_spec.rb
@@ -16,10 +16,8 @@ describe UpdateDomainWorker do
         .and_return(resource)
       allow(resource).to receive(:get).and_return(html_content)
 
-      expect {
-        described_class.new.perform(domain.id)
-      }.to change { domain.reload.meta }.from(nil).to("Get structured data out of the web.")
-       .and change { domain.reload.title }.from(nil).to("morph.io")
+      expect { described_class.new.perform(domain.id) }.to change { domain.reload.meta }.from(nil).to("Get structured data out of the web.")
+                                                           .and change { domain.reload.title }.from(nil).to("morph.io")
     end
   end
 end
